a4438bbd5c081d8f272ecf958cd602d730703e7d
Pengsha Ying <810998652@qq.com>
2021-05-27 18:37:21 +0800

    feat(mainProcess): hot updates

--- src/main/vite.config.js
+++ src/main/vite.config.js
@@ -5,9 +5,6 @@ import autoExternal from 'rollup-plugin-auto-external';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    emptyOutDir: false,
-    outDir: '../../dist',
-    watch: true,
     lib: {
       entry: path.resolve(__dirname, './index.ts'),
       name: 'main',
